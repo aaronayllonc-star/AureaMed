70e37963f4a4e42fde78c29e926fabe8f0dda2c7
Aarón Ayllon C <aaronayllonc@gmail.com>
2025-11-03 15:33:20 -0400

    mi primer commit

---
+++ inicio/script.js
@@ -0,0 +1,17 @@
+function enviarFormulario(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    fetch('guardar_comentario.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.body.innerHTML = data;
+    })
+    .catch(error => {
+        alert('❌ Error: ' + error);
+    });
+}
+
